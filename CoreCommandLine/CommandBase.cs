cdcdeec500685bd13618de6bc9916420306a042d
Mani Moayedi <acidmanic.moayedi.com>
2023-02-13 13:44:41 +0330

    Add some documentation to CommandBase

--- CoreCommandLine/CommandBase.cs
+++ CoreCommandLine/CommandBase.cs
@@ -35,7 +35,7 @@ namespace CoreCommandLine
 
         protected Action<String> Output { get; private set; } = Console.WriteLine;
 
-        
+
         public void SetLogger(ILogger logger)
         {
             Logger = logger;
@@ -46,6 +46,15 @@ namespace CoreCommandLine
             Output = output;
         }
 
+        /// <summary>
+        /// Given a set of inputs (string[] args) this method grabs the first one as command name,
+        /// and the reset as arguments of the command.
+        /// </summary>
+        /// <param name="args">input arguments to be processed</param>
+        /// <returns>
+        /// an instance of CommandArguments structure containing the command name and it's arguments,
+        /// wrapped in a Result class.
+        /// </returns>
         protected Result<CommandArguments> GetCommandArguments(string[] args)
         {
             if (args.Length == 0)
@@ -91,6 +100,14 @@ namespace CoreCommandLine
             return CommandUtilities.AreNamesEqual(name1, name2);
         }
 
+        /// <summary>
+        /// Determines if the given string[]args starts with current commands .
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="args">input arguments to be processed</param>
+        /// <returns>
+        /// True if this set of inputs should result in execution of current command. Otherwise returns False.
+        /// </returns>
         protected bool IsThisSetMyCommand(Context context, string[] args)
         {
             var arguments = GetCommandArguments(args);
@@ -106,11 +123,22 @@ namespace CoreCommandLine
             return false;
         }
 
+        /// <summary>
+        /// Searches the args[] array to see if current command is appeared. 
+        /// </summary>
+        /// <param name="args">input arguments to be processed</param>
+        /// <returns>True if current command name is mentioned in inputs. Otherwise returns False.</returns>
         protected bool AmIPresent(string[] args)
         {
             return IndexOf(NameBundle, args) > -1;
         }
 
+        /// <summary>
+        /// For Argument Commands, Searches the given set of input arguments for any occurrence of current command.
+        /// If found any, the next input would be considered as the value user entered for this command. 
+        /// </summary>
+        /// <param name="args">input arguments to be processed</param>
+        /// <returns>Successful Result containing the found value. Otherwise returns a Failure Result.</returns>
         protected Result<string> FindMyValue(string[] args)
         {
             var keyIndex = IndexOf(NameBundle, args);
@@ -118,7 +146,7 @@ namespace CoreCommandLine
             if (keyIndex > -1)
             {
                 var valueIndex = keyIndex + 1;
-                
+
                 if (args.Length > valueIndex)
                 {
                     return new Result<string>(true, args[valueIndex]);
